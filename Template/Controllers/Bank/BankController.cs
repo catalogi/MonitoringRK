c926b54055ce768514cc5942cae799c5e1a7c2b9
RoySianipar <roysianipar418@gmail.com>
2022-09-01 14:51:56 +0700

    up

---
+++ Template/Controllers/Bank/BankController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASK_Core.Controllers.Bank
+{
+    public class BankController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
